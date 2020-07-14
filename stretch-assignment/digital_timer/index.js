93babf539bf5b740ff7790c24822564d092fac14
EmilyA <emilyadams.3d@gmail.com>
2020-07-14 13:16:58 -0700

    finished stretch

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,20 +1,49 @@
-let seconds = document.getElementsByClassName("secondOnes");
-seconds.window.setInterval(countSeconds, 1000)
-seconds.textContent(countSeconds)
-
-let ms = document.getElementsByClassName("msTens");
-ms.window.setInterval(countMS, 1)
-
-countSeconds();{
-    if (i<10) {
-        i = 1+i;
-        return i;
+// SECONDS 
+setInterval(changeSeconds, 1000);
+let seconds = 0;
+document.getElementById("secondTens").textContent = 0;
+document.getElementById("secondOnes").textContent = 0;
+function changeSeconds(){
+    if (seconds < 11){
+        document.getElementById("secondOnes").textContent = seconds++;
+    }
+    if (seconds == 11){
+        document.getElementById("secondOnes").textContent = 0;
+        return document.getElementById("secondTens").textContent = 1;
     }
 }
 
-countMS();{
-    if (i<1000){
-        i = 1+i;
-        return i; 
+setInterval(changeMS, 1);
+let ms = 0;
+let msHundred = 0;
+document.getElementById("msHundreds").textContent = 1;
+document.getElementById("msTens").textContent = 0;
+
+function changeMS(){
+    if (document.getElementById("secondTens").textContent == 0){
+    if (ms < 10){
+        document.getElementById("msTens").textContent = ms++;
+    }
+    else if (ms === 10){
+        document.getElementById("msTens").textContent = 0;
+        ms = 0;
+        document.getElementById("msHundreds").textContent = msHundred++;
+        if(msHundred === 6){
+            msHundred = 0;
+        }
     }
 }
+    if (document.getElementById("secondTens").textContent == !0){
+        document.getElementById("msHundreds").textContent = 0;
+        document.getElementById("msTens").textContent = 0;
+    }
+}
+
+// let secondOnes & msTens = 0;
+// if secondOnes or msTens < 10
+// secondOnes++ msTens++;
+// if msTens == 60 then msTens = 0;
+
+//let msTens = document.getElementsByClassName("msTens");
+// if secondOnes = 10 then secondTens++; 
+// if msTens = 60 then msHundreds++;
